78c06d135fc61aaa621b0a85fb629291e81b39f2
miyacoz <z@zeppel.biz>
2021-11-07 19:07:47 +0000

    extracted lib.js

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
 require('dotenv').config()
 const { readFile } = require('fs/promises')
 const { env, exit } = require('process')
-const { spawn: nonPromiseSpawn } = require('child_process')
-
-const round = value => {
-  let digits = 2
-  if (value > 100) {
-    digits = 0
-  } else if (value > 10) {
-    digits = 1
-  }
-
-  return Math.floor(value * Math.pow(10, digits)) / Math.pow(10, digits)
-}
-
-const convertUnit = value => {
-  let unit = 'kb'
-
-  if (value / 1024 >= 1) {
-    value /= 1024
-    unit = 'mb'
-
-    if (value / 1024 >= 1) {
-      value /= 1024
-      unit = 'gb'
-    }
-  }
-
-  return { value: round(value), unit }
-}
-
-const spawn = command => new Promise((s, j) => {
-  const child = nonPromiseSpawn(command)
-  child.stdout.on('data', data => s(String(data)))
-  child.stderr.on('data', data => j(String(data)))
-  child.on('close', code => s(String(code)))
-})
+const { convertUnit, spawn } = require('./lib.js')
 
 ;(async () => {
   try {
---
+++ lib.js
@@ -0,0 +1,40 @@
+const { spawn: nonPromiseSpawn } = require('child_process')
+
+const round = value => {
+  let digits = 2
+  if (value > 100) {
+    digits = 0
+  } else if (value > 10) {
+    digits = 1
+  }
+
+  return Math.floor(value * Math.pow(10, digits)) / Math.pow(10, digits)
+}
+
+const convertUnit = value => {
+  let unit = 'kb'
+
+  if (value / 1024 >= 1) {
+    value /= 1024
+    unit = 'mb'
+
+    if (value / 1024 >= 1) {
+      value /= 1024
+      unit = 'gb'
+    }
+  }
+
+  return { value: round(value), unit }
+}
+
+const spawn = command => new Promise((s, j) => {
+  const child = nonPromiseSpawn(command)
+  child.stdout.on('data', data => s(String(data)))
+  child.stderr.on('data', data => j(String(data)))
+  child.on('close', code => s(String(code)))
+})
+
+module.exports = {
+  convertUnit,
+  spawn,
+}
