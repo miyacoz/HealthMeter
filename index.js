284590b702e8e894ef624e43f7f368888fc9b7d0
miyacoz <z@zeppel.biz>
2021-11-07 18:50:59 +0000

    added logic to display disk usage

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const convertUnit = value => {
 
 (async () => {
   try {
+    /* memory */
     const meminfo = await readFile('/proc/meminfo', { encoding: 'utf8' })
     const lines = meminfo
       .split('\n')
@@ -54,6 +55,7 @@ const convertUnit = value => {
     let memoryUsed = parsed.memtotal - parsed.memavailable
     let swapUsed = parsed.swaptotal - parsed.swapfree
 
+    /* cpu */
     const askLoadAverage = () => new Promise((s, j) => {
       const uptime = spawn('uptime')
       uptime.stdout.on('data', data => s(data))
@@ -67,6 +69,26 @@ const convertUnit = value => {
       .split(',')
       .map(v => parseFloat(v, 10))
 
+    /* disk */
+    const askDiskUsage = () => new Promise((s, j) => {
+      const df = spawn('df')
+      df.stdout.on('data', data => s(data))
+      df.stderr.on('data', data => j(data))
+      df.on('close', code => s(code))
+    })
+
+    const dfResult = String(await askDiskUsage())
+    const { total: diskTotal, used: diskUsed } = dfResult
+      .trim()
+      .split('\n')
+      .filter((l, i) => i !== 0)
+      .map(l => {
+        const [, total, used] = l.replace(/\s+/g, ' ').split(' ')
+        return [parseInt(total, 10), parseInt(used, 10)]
+      })
+      .reduce(({ total, used }, [currentTotal, currentUsed]) => ({ total: total + currentTotal, used: used + currentUsed }), { total: 0, used: 0 })
+    const diskUsePercent = Math.ceil(diskUsed * 100 / diskTotal)
+
     console.log(
       convertUnit(parsed.memtotal),
       convertUnit(memoryUsed),
@@ -74,7 +96,10 @@ const convertUnit = value => {
       convertUnit(parsed.swaptotal),
       convertUnit(swapUsed),
       swapUsePercent,
-      loadAverage
+      loadAverage,
+      convertUnit(diskTotal),
+      convertUnit(diskUsed),
+      diskUsePercent
     )
     exit(0)
   } catch (e) {
