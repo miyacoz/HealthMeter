a293331438ea7864edd9319f7bcc5b9768ff6823
miyacoz <z@zeppel.biz>
2021-11-07 07:44:34 +0000

    added logic to display memory usage

--- index.js
+++ index.js
@@ -1,3 +1,71 @@
 require('dotenv').config()
+const { readFile } = require('fs/promises')
+const { env, exit } = require('process')
 
-console.log(process.env.WEBHOOK_URL)
+const decimal = (value, digits = 2) => Math.floor(value * Math.pow(10, digits)) / Math.pow(10, digits)
+
+const round = kb => {
+  let value = kb
+  let unit = 'kb'
+  let mb = 0
+  let gb = 0
+
+  if (value / 1024 >= 1) {
+    value /= 1024
+    unit = 'mb'
+
+    if (value / 1024 >= 1) {
+      value /= 1024
+      unit = 'gb'
+    }
+  }
+
+  if (value > 100) {
+    value = decimal(value, 0)
+  } else if (value > 10) {
+    value = decimal(value, 1)
+  } else {
+    value = decimal(value)
+  }
+
+  return { value, unit }
+}
+
+(async () => {
+  try {
+    const meminfo = await readFile('/proc/meminfo', { encoding: 'utf8' })
+    const lines = meminfo
+      .split('\n')
+      .filter(l => /memtotal|memavailable|swaptotal|swapfree/i.test(l))
+
+    if (lines.length !== 4) {
+      throw new Error(`Number of retreated lines from proc meminfo is not 4: ${lines.length}`)
+    }
+
+    const parsed = {}
+    lines
+      .map(l => l.toLowerCase().split(':'))
+      .forEach(([title, value]) => {
+        parsed[title] = parseInt(value.trim().split(' ')[0], 10)
+      })
+
+    const memoryUsePercent = Math.ceil(100 - (parsed.memavailable * 100 / parsed.memtotal))
+    const swapUsePercent = Math.ceil(100 - (parsed.swapfree * 100 / parsed.swaptotal))
+
+    let memoryUsed = parsed.memtotal - parsed.memavailable
+    let swapUsed = parsed.swaptotal - parsed.swapfree
+
+    console.log(
+      round(parsed.memtotal),
+      round(memoryUsed),
+      memoryUsePercent,
+      round(parsed.swaptotal),
+      round(swapUsed),
+      swapUsePercent
+    )
+    exit(0)
+  } catch (e) {
+    console.warn(e)
+    exit(127)
+  }
+})()
